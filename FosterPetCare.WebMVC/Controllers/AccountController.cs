17367d5f27067819296c4fa80db5e5f497b7e60f
savannahlromero <savannahlromero@gmail.com>
2019-02-20 10:35:10 -0500

    Finished fixing the roles. Starting the styling process.

--- FosterPetCare.WebMVC/Controllers/AccountController.cs
+++ FosterPetCare.WebMVC/Controllers/AccountController.cs
@@ -20,6 +20,7 @@ namespace FosterPetCare.WebMVC.Controllers
         private ApplicationUserManager _userManager;
 
         ApplicationDbContext context;
+
         public AccountController()
         {
             context = new ApplicationDbContext();
@@ -159,16 +160,17 @@ namespace FosterPetCare.WebMVC.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    await this.UserManager.AddToRoleAsync(user.Id, model.UserRoles);
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
                     return RedirectToAction("Index", "Home");
                 }
+                ViewBag.Name = new SelectList(context.Roles.Where(u => !u.Name.Contains("Admin") && !u.Name.Contains("Staff")).ToList(), "Name", "Name");
                 AddErrors(result);
             }
 
